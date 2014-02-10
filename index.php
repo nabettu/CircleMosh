b11bdcd7b038663a798e141b99f6f64caea51bed
kawakami takahiro <kawakami@kawakami-no-MacBook-Pro.local>
2014-02-11 00:45:38 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+	<?php get_header(); ?>
+	<!-- blog -->
+	<div id="blog">
+	
+		<article>
+			<header>
+				<h1 class="title"><a href="single.html" title="">PHPの入門で読んだ本の紹介</a></h1>
+			</header>
+			<div class="entrySummary centering">
+				<a href="#"><img src="img/icatch.jpg"></a>
+				<p>WEBアプリに関してはPHPでつくって行こうと思って入門書を数冊読み漁ったのでそれらの紹介です。まだ全然ゴリゴリ書いている訳ではないので、素人感から抜け出せてはないですが、書いていきます。 基礎からの</p>
+				<ul class="summaryMeta clearfix">
+					<li><a href="#" title="「PHPの入門で読んだ本の紹介」の続きを読む">続きを読む</a></li>
+					<li>カテゴリー： <a href="#" title="PHP の投稿をすべて表示" rel="category tag">PHP</a> <a href="#" title="WEB開発 の投稿をすべて表示" rel="category tag">WEB開発</a></li>
+				</ul>
+			</div>
+		</article>
+
+	</div>
+	<!-- page -->
+	<div class='wp-pagenavi'>
+		（ページ送りスペース）
+	</div>
+	<?php get_footer(); ?>
