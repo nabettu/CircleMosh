a7886c37e87c41e7442b7ef022680e55a7090656
tatsuaki_w <tatsuaki.w@gmail.com>
2014-06-30 03:14:21 +0900

    sp対応

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
 			<h1 class="title">Category</h1>
 			<ul>
 				<li><a href="http://blog.t-tu.com/category/ios%E3%82%A2%E3%83%97%E3%83%AA/"><img src="<?php bloginfo('template_url'); ?>/img/apple.png"><br>アプリ</a></li>
-				<li><a href="http://blog.t-tu.com/category/web%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9/"><img src="<?php bloginfo('template_url'); ?>/img/earth.png"><br>Webサービス</a></li>
+				<li><a href="http://blog.t-tu.com/category/web%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9/"><img src="<?php bloginfo('template_url'); ?>/img/earth.png"><br><nobr>Webサービス</nobr></a></li>
 				<li><a href="http://blog.t-tu.com/category/%E7%B4%B9%E4%BB%8B/"><img src="<?php bloginfo('template_url'); ?>/img/radio.png"><br>紹介</a></li>
 				<li><a href="http://blog.t-tu.com/category/wordpress/"><img src="<?php bloginfo('template_url'); ?>/img/wp.png"><br>Wordpress</a></li>
-				<li><a href="http://blog.t-tu.com/category/%E6%97%A5%E5%B8%B8/"><img src="<?php bloginfo('template_url'); ?>/img/light.png"><br>日常・その他</a></li>
+				<li><a href="http://blog.t-tu.com/category/思考/"><img src="<?php bloginfo('template_url'); ?>/img/light.png"><br><nobr>日常・その他</nobr></a></li>
 			</ul>
 		</section>
 	</div>
